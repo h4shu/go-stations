f5b9eb5722b8434f54049ec43b33e5d4795d3afc
hashu <yuta.hassi@gmail.com>
2023-06-08 22:44:42 +0900

    Go 15

--- service/todo.go
+++ service/todo.go
@@ -53,7 +53,32 @@ func (s *TODOService) ReadTODO(ctx context.Context, prevID, size int64) ([]*mode
 		readWithID = `SELECT id, subject, description, created_at, updated_at FROM todos WHERE id < ? ORDER BY id DESC LIMIT ?`
 	)
 
-	return nil, nil
+	var (
+		rows *sql.Rows
+		err error
+	)
+	if prevID == 0 {
+		rows, err = s.db.QueryContext(ctx, read, size)
+	} else {
+		rows, err = s.db.QueryContext(ctx, readWithID, prevID, size)
+	}
+	
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	todos := make([]*model.TODO, 0)
+	for rows.Next() {
+		var todo model.TODO
+		err = rows.Scan(&todo.ID, &todo.Subject, &todo.Description, &todo.CreatedAt, &todo.UpdatedAt)
+		if err != nil {
+			return nil, err
+		}
+		todos = append(todos, &todo)
+	}
+
+	return todos, nil
 }
 
 // UpdateTODO updates the TODO on DB.
