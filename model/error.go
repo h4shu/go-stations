c6b0d34a53ca358bfcbe8eb401d380067012b244
hashu <yuta.hassi@gmail.com>
2023-06-08 17:12:33 +0900

    Go 10

---
+++ model/error.go
@@ -0,0 +1,23 @@
+package model
+
+import (
+	"fmt"
+	"time"
+)
+
+type ErrNotFound struct {
+	When time.Time
+	What string
+}
+
+func (e *ErrNotFound) Error() string {
+	return fmt.Sprintf("at %v, %s",
+		e.When, e.What)
+}
+
+func run() error {
+	return &ErrNotFound{
+		time.Now(),
+		"Not found.",
+	}
+}
