8474895ce494f9eb71395e7ee07f787c07564800
hashu <yuta.hassi@gmail.com>
2023-06-08 19:42:21 +0900

    Go 13

--- handler/todo.go
+++ handler/todo.go
@@ -39,8 +39,11 @@ func (h *TODOHandler) Read(ctx context.Context, req *model.ReadTODORequest) (*mo
 
 // Update handles the endpoint that updates the TODO.
 func (h *TODOHandler) Update(ctx context.Context, req *model.UpdateTODORequest) (*model.UpdateTODOResponse, error) {
-	_, _ = h.svc.UpdateTODO(ctx, 0, "", "")
-	return &model.UpdateTODOResponse{}, nil
+	todo, err := h.svc.UpdateTODO(ctx, req.ID, req.Subject, req.Description)
+	if err != nil {
+		return nil, err
+	}
+	return &model.UpdateTODOResponse{TODO: *todo}, nil
 }
 
 // Delete handles the endpoint that deletes the TODOs.
@@ -68,6 +71,30 @@ func (h *TODOHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		err = json.NewEncoder(w).Encode(cres)
+		if err != nil {
+			log.Println(err)
+		}
+	} else if r.Method == "PUT" {
+		var u model.UpdateTODORequest
+		err := json.NewDecoder(r.Body).Decode(&u)
+		if err != nil {
+			log.Println(err)
+			return
+		} else if (u.Subject == "") {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		ures, err := h.Update(ctx, &u)
+		if err != nil {
+			switch err.(type) {
+			case *model.ErrNotFound:
+				w.WriteHeader(http.StatusNotFound)
+			default:
+				log.Println(err)
+			}
+			return
+		}
+		err = json.NewEncoder(w).Encode(ures)
 		if err != nil {
 			log.Println(err)
 		}
